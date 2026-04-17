5f3eb00016d550a9d7d8fe77f689a741d1ccfb2f
Sen <haizh310@gmail.com>
2026-04-17 17:25:37 +0000

    Apr 17

---
+++ js-events/script.js
@@ -0,0 +1,21 @@
+function greet(name) {
+    console.log("Hello, "+ name);
+}
+
+greet("Miku");
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    alert("Sum is equal to: "+ sum);
+}
+//addNumbers(2, 4);
+
+function articleFaith1() {
+    alert("We believe in God, the Eternal Father, and in His Son, Jesus Christ, and in the Holy Ghost.");
+}
+function articleFaith2() {
+    alert("We believe that men will be punished for their own sins, and not for Adam’s transgression.");
+}
+function articleFaith3() {
+    alert("We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.");
+}
