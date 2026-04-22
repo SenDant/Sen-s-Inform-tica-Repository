de8b19183255dd481b51dd7ee5626c23c14275b2
Sen <haizh310@gmail.com>
2026-04-22 20:02:49 +0000

    Apr 22

--- js-variables/script.js
+++ js-variables/script.js
@@ -5,7 +5,7 @@ function function1() {
     let mood = "silly";
     console.log("User information:");
     console.log(name, favcolor, location, mood);
-    let mood = sillier;
+    mood = "sillier";
     console.log("User information:");
     console.log(name, favcolor, location, mood);
 }
@@ -13,14 +13,14 @@ function function2() {
     const companyName = "Re-Logic";
     const foundingYear = 2011;
     const isActive = true;
-    let fundingAmount =
+    let fundingAmount;
     console.log(companyName, foundingYear, isActive, fundingAmount);
 }
 function function3() {
-    let anyNumber = 9;
-    anyNumber = anyNumber*anyNumber;
-    anyNumber = anyNumber+10;
-    anynumber = anyNumber/2;
-    anyNumber = anyNumber-9;
-    alert(anyNumber);
+    let n = 9;
+    n = n * 2;
+    n = n + 10;
+    n = n / 2;
+    n = n - 9;
+    alert(n);
 }
