2d8a314ec868edf85f01c8ad7f933b88d1f16225
Sen <haizh310@gmail.com>
2026-04-27 20:19:02 +0000

    Apr 27

---
+++ js-coin/script.js
@@ -0,0 +1,12 @@
+function flip() {
+    result = Math.random();
+    if (result < 0.5) {
+        coin = "Tails"
+    }
+    else {
+        coin = "Heads!"
+    }
+    let heading = document.querySelector('h2');
+	heading.innerHTML = coin;
+    console.log(result);
+}
