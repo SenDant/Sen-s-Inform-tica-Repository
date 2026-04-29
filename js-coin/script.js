9faef7fe70379bd3141194c3044d06de18be6e78
Sen <haizh310@gmail.com>
2026-04-29 20:19:15 +0000

    Apr 29

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,4 +1,25 @@
-function flip() {
+function isItTails(guess, coin) {
+    guess = "Tails!";
+    return guess;
+}
+
+function isItHeads(guess, coin) {
+    guess = "Heads!";
+    return guess;
+}
+
+function rightOrWrong() {
+     if (coin = guess) {
+        let guessed = document.querySelector('h3');
+	    guessed.innerHTML = "correct answer. YOU WIN!";
+    }
+    else {
+        let guessed = document.querySelector('h3');
+	    guessed.innerHTML = "wrong answer. YOU LOSE!";
+    }
+}
+
+function flip(coin) {
     result = Math.random();
     if (result < 0.5) {
         coin = "Tails!"
@@ -9,4 +30,11 @@ function flip() {
     let heading = document.querySelector('h2');
 	heading.innerHTML = coin;
     console.log(result);
+    return coin;
+}
+function restart() {
+    let heading = document.querySelector('h2');
+	heading.innerHTML = "";
+    let guessed = document.querySelector('h3');
+	guessed.innerHTML = "";
 }
