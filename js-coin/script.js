72649986bec1bd3032b876faa8503c7a3f59f954
Sen <haizh310@gmail.com>
2026-04-30 01:54:30 +0000

    Apr 29.1

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,25 +1,8 @@
+let guess;
+let coin;
 function isItTails(guess, coin) {
     guess = "Tails!";
-    return guess;
-}
-
-function isItHeads(guess, coin) {
-    guess = "Heads!";
-    return guess;
-}
-
-function rightOrWrong() {
-     if (coin = guess) {
-        let guessed = document.querySelector('h3');
-	    guessed.innerHTML = "correct answer. YOU WIN!";
-    }
-    else {
-        let guessed = document.querySelector('h3');
-	    guessed.innerHTML = "wrong answer. YOU LOSE!";
-    }
-}
-
-function flip(coin) {
+    ////////////////////// GET THE COIN'S FACE
     result = Math.random();
     if (result < 0.5) {
         coin = "Tails!"
@@ -28,13 +11,92 @@ function flip(coin) {
         coin = "Heads!"
     }
     let heading = document.querySelector('h2');
-	heading.innerHTML = coin;
+    heading.innerHTML = coin;
+    console.log("you chose Tails.");
     console.log(result);
-    return coin;
+    console.log(coin);
+    ////////////////////////// IS THE GUESS RIGHT OR WRONG?
+    if (coin === guess) {
+        let guessed = document.querySelector('h3');
+	    guessed.innerHTML = "correct answer. YOU WIN!";
+        console.log("Won");
+        return 1;
+    }
+    else {
+        let guessed = document.querySelector('h3');
+	    guessed.innerHTML = "wrong answer. YOU LOSE!";
+        console.log("Lost");
+        return 2;
+    }
 }
-function restart() {
+
+function isItHeads(guess, coin) {
+   guess = "Heads!";
+    ////////////////////// GET THE COIN'S FACE
+    result = Math.random();
+    if (result < 0.5) {
+        coin = "Tails!"
+    }
+    else {
+        coin = "Heads!"
+    }
+    let heading = document.querySelector('h2');
+    heading.innerHTML = coin;
+
+    console.log("you chose Heads.");
+    console.log(result);
+    console.log(coin);
+    ////////////////////////// IS THE GUESS RIGHT OR WRONG?
+   if (coin === guess) {
+        let guessed = document.querySelector('h3');
+	    guessed.innerHTML = "correct answer. YOU WIN!";
+        console.log("Won");
+        return "won";
+    }
+    else {
+        let guessed = document.querySelector('h3');
+	    guessed.innerHTML = "wrong answer. YOU LOSE!";
+        console.log("Lost");
+        return "lost";
+    }
+}
+
+let headsguesses = isItHeads();
+let tailsguesses = isItTails();
+let wins = 0;
+let loses = 0;
+
+function ratio(wins, loses, headsguesses, tailsguesses) {
+    if (headsguesses === "won") {
+        wins++;
+    }
+    if (tailsguesses === "won") {
+        wins++;
+    }
+    if (headsguesses === "lost") {
+        loses++;
+    }
+    if (tailsguessess === "lost") {
+        loses++;
+    }
+    let victories = document.querySelector('span#wins');
+	victories.innerHTML = wins;
+    let defeats = document.querySelector('span#loses');
+	defeats.innerHTML = loses;
+}
+
+function restart(coin, result, wins, loses) {
     let heading = document.querySelector('h2');
 	heading.innerHTML = "";
     let guessed = document.querySelector('h3');
 	guessed.innerHTML = "";
+    let ratiowins = document.querySelector('span#wins');
+	ratiowins.innerHTML = "0";
+    let ratioloses = document.querySelector('span#loses');
+	ratioloses.innerHTML = "0";
+    coin = undefined;
+    result = undefined;
+    wins = 0;
+    loses = 0;
+    console.log(coin, result, wins, loses);
 }
