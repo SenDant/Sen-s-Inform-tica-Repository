f4837800def6f410fbf937662131e7ec6ba10991
Sen <haizh310@gmail.com>
2026-05-12 16:38:25 +0000

    May 12

--- interval/script.js
+++ interval/script.js
@@ -1,15 +1,20 @@
 let counter = 0;
-
+let alarm = new Audio("sound/alarm.mp3");
 function count(t) {
+    console.log(t);
     let start = t*60;
-    counter--;
-    time = start + counter;
+    counter++;
+    time = start - counter;
     document.querySelector('h1').innerHTML = time;
+    if (time == 0) {
+        alarm.currentTime = 0;
+        alarm.play();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = function(){
-    setInterval(count, 1000); // It's a 1,000 because it's measured in miliseconds.
-    document.querySelector("button").disabled = true;
-    };
+        setInterval(count, 1000); // It's a 1,000 because it's measured in miliseconds.
+        document.querySelector("button").disabled = true;
+        };
 });
